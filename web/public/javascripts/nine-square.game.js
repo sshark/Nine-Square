20f0e499c7d7213d767daf3d9fd36a85251ec7a1
Lim, Teck Hooi <teck-hooi.lim@hp.com>
2015-04-20 19:01:49 +0800

    1. Removed numpad input style. 2. Added keyboard input support with arrow keys for navigation and number keys for input. 3. Provided on screen numbers to support number input using mouse.

--- web/public/javascripts/nine-square.game.js
+++ web/public/javascripts/nine-square.game.js
@@ -14,13 +14,6 @@ $(document).ready(function () {
         }
     });
 
-    var numpadDialog = $("#numpad-dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        dialogClass: "numpad-dialog",
-        width: 154
-    });
-
     $(".easy-level-btn").click(function () {
         loadBoardFor("easy");
     });
@@ -122,109 +115,99 @@ $(document).ready(function () {
         addNewCellsTo(board);
     });
 
-
-    function PosXY(x, y) {
-        this.x = x;
-        this.y = y;
-
-        this.incX = function () {
-            if (x + 1 > 8) {
-                return new PosXY(0, y);
-            }
-            else {
-                return new PosXY(x + 1, y);
-            }
-        }
-
-        this.decX = function () {
-            if (x - 1 < 0) {
-                return new PosXY(8, y);
-            }
-            return new PosXY(x - 1, y);
-        }
-
-        this.incY = function () {
-            if (y + 1 > 8) {
-                return new PosXY(x, 0);
-            }
-            else {
-                return new PosXY(x, y + 1);
-            }
-        }
-
-        this.decY = function () {
-            if (y - 1 < 0) {
-                return new PosXY(x, 8)
-            }
-            else {
-                return new PosXY(x, y - 1);
-            }
-        }
-
-        this.toNdx = function () {
-            return this.x + this.y * 9;
-        }
-    }
-
-    function toXYPos(ndx) {
-        return new PosXY(ndx % 9, Math.floor(ndx / 9));
-    }
-
-    var cellId = 0;
-    $("#" + 40).css('background-color', 'red');
     $(document).keydown(function (e) {
         switch (e.which) {
-            case 37:
-                var newXYPos = toXYPos(cellId).decX();
+            case 37: // left
+                var newXYPos = nextAvailableCell(cellId, LEFT);
                 $('#' + newXYPos.toNdx()).css('background-color', 'red');
                 $('#' + cellId).css('background-color', '');
                 cellId = newXYPos.toNdx();
-                console.log("going left");
                 break;
 
-            case 38:
-                var newXYPos = toXYPos(cellId).decY();
+            case 38: // up
+                var newXYPos = nextAvailableCell(cellId, UP);
                 $('#' + newXYPos.toNdx()).css('background-color', 'red');
                 $('#' + cellId).css('background-color', '');
                 cellId = newXYPos.toNdx();
-                console.log("going up");
                 break;
 
             case 39: // right
-                var newXYPos = toXYPos(cellId).incX();
+                var newXYPos = nextAvailableCell(cellId, RIGHT);
                 $('#' + newXYPos.toNdx()).css('background-color', 'red');
                 $('#' + cellId).css('background-color', '');
                 cellId = newXYPos.toNdx();
-                console.log("going right");
                 break;
 
             case 40: // down
-                var newXYPos = toXYPos(cellId).incY();
+                var newXYPos = nextAvailableCell(cellId, DOWN);
                 $('#' + newXYPos.toNdx()).css('background-color', 'red');
                 $('#' + cellId).css('background-color', '');
                 cellId = newXYPos.toNdx();
-                console.log("going down");
+                break;
+
+            case 49: // 1
+                $('#' + cellId).html('1');
+                break;
+
+            case 50: // 2
+                $('#' + cellId).html('2');
+                break;
+
+            case 51: // 3
+                $('#' + cellId).html('3');
+                break;
+
+            case 52: // 4
+                $('#' + cellId).html('4');
+                break;
+
+            case 53: // 5
+                $('#' + cellId).html('5');
+                break;
+
+            case 54: // 6
+                $('#' + cellId).html('6');
+                break;
+
+            case 55: // 7
+                $('#' + cellId).html('7');
+                break;
+
+            case 56: // 8
+                $('#' + cellId).html('8');
+                break;
+
+            case 57: // 9
+                $('#' + cellId).html('9');
+                break;
+
+            case 67: // C
+                $('#' + cellId).html('&nbsp;');
                 break;
 
             default:
+                console.log(e.which + " pressed.");
                 return; // exit this handler for other keys
         }
         e.preventDefault(); // prevent the default action (scroll / move caret)
     });
 
-    $(".one-btn").click(numpadNumericFunGen(numpadDialog, "1"));
-    $(".two-btn").click(numpadNumericFunGen(numpadDialog, "2"));
-    $(".three-btn").click(numpadNumericFunGen(numpadDialog, "3"));
-    $(".four-btn").click(numpadNumericFunGen(numpadDialog, "4"));
-    $(".five-btn").click(numpadNumericFunGen(numpadDialog, "5"));
-    $(".six-btn").click(numpadNumericFunGen(numpadDialog, "6"));
-    $(".seven-btn").click(numpadNumericFunGen(numpadDialog, "7"));
-    $(".eight-btn").click(numpadNumericFunGen(numpadDialog, "8"));
-    $(".nine-btn").click(numpadNumericFunGen(numpadDialog, "9"));
-    $(".clear-btn").click(numpadNumericFunGen(numpadDialog, " "));
-    $(".close-btn").click(function () {
-        numpadDialog.dialog("close")
-    });
+    $(".btn-1").click(assignToCell("1"));
+    $(".btn-2").click(assignToCell("2"));
+    $(".btn-3").click(assignToCell("3"));
+    $(".btn-4").click(assignToCell("4"));
+    $(".btn-5").click(assignToCell("5"));
+    $(".btn-6").click(assignToCell("6"));
+    $(".btn-7").click(assignToCell("7"));
+    $(".btn-8").click(assignToCell("8"));
+    $(".btn-9").click(assignToCell("9"));
+    $(".btn-clear").click(assignToCell("&nbsp;"));
+
+    function  assignToCell(c) {
+        return function() {
+            $("#" + cellId).html(c);
+        }
+    }
 
     $(".exit-to-main-btn").click(function () {
         $(".nine-square-panel").effect("explode", {}, 500, function () {
@@ -233,6 +216,98 @@ $(document).ready(function () {
         });
     });
 });
+
+var UP = 0;
+var RIGHT = 1;
+var DOWN = 2;
+var LEFT = 3;
+
+var cellId = 41;
+
+function PosXY(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.incX = function () {
+        if (x + 1 > 8) {
+            return new PosXY(0, y);
+        }
+        else {
+            return new PosXY(x + 1, y);
+        }
+    }
+
+    this.decX = function () {
+        if (x - 1 < 0) {
+            return new PosXY(8, y);
+        }
+        return new PosXY(x - 1, y);
+    }
+
+    this.incY = function () {
+        if (y + 1 > 8) {
+            return new PosXY(x, 0);
+        }
+        else {
+            return new PosXY(x, y + 1);
+        }
+    }
+
+    this.decY = function () {
+        if (y - 1 < 0) {
+            return new PosXY(x, 8)
+        }
+        else {
+            return new PosXY(x, y - 1);
+        }
+    }
+
+    this.toNdx = function () {
+        return this.x + this.y * 9;
+    }
+}
+
+function nextAvailableCell(ndx, dir) {
+    var pos = toXYPos(ndx);
+    switch(dir) {
+        case UP:
+            var newPos = pos.decY();
+            if ($("#" + newPos.toNdx()).attr("class").indexOf("seed") < 0) {
+                return newPos;
+            } else {
+                return nextAvailableCell(newPos.toNdx(), dir);
+            }
+            break;
+        case RIGHT:
+            var newPos = pos.incX();
+            if ($("#" + newPos.toNdx()).attr("class").indexOf("seed") < 0) {
+                return newPos;
+            } else {
+                return nextAvailableCell(newPos.toNdx(), dir);
+            }
+            break;
+        case DOWN:
+            var newPos = pos.incY();
+            if ($("#" + newPos.toNdx()).attr("class").indexOf("seed") < 0) {
+                return newPos;
+            } else {
+                return nextAvailableCell(newPos.toNdx(), dir);
+            }
+            break;
+        case LEFT:
+            var newPos = pos.decX();
+            if ($("#" + newPos.toNdx()).attr("class").indexOf("seed") < 0) {
+                return newPos;
+            } else {
+                return nextAvailableCell(newPos.toNdx(), dir);
+            }
+            break;
+    }
+}
+
+function toXYPos(ndx) {
+    return new PosXY(ndx % 9, Math.floor(ndx / 9));
+}
 
 function _dialog(title, message, onClose) {
     $("<div id=\"dialog-message\"></div>")
@@ -252,14 +327,6 @@ function _dialog(title, message, onClose) {
         });
 }
 
-function numpadNumericFunGen(numpadDialog, c) {
-    return function () {
-        var cell = $(".game-board").children("#" + numpadDialog.data("id"));
-        " " == c ? cell.html("&nbsp;") : cell.text(c);
-        numpadDialog.dialog("close");
-    }
-}
-
 function buildListFrom(board, buildEmptyPuzzle) {
     return board.children().map(function () {
         return (!buildEmptyPuzzle || $(this).hasClass("seed")) ? guardedParseInt($(this).text()) : 0
@@ -274,6 +341,8 @@ function loadBoardFor(level) {
     $("#set-level-dialog").dialog("close");
     addNewCellsTo($(".game-board"));
     $(".nine-square-panel").effect("fade", {}, 1000, function () {
+        cellId = nextAvailableCell(cellId, LEFT).toNdx();
+        $("#" + cellId).css('background-color', 'red');
     });
 }
 
@@ -292,9 +361,13 @@ function addNewCellsTo(board) {
                 div.addClass("seed");
             } else {
                 div.addClass("empty");
-                div.click(function () {
-                    $("#numpad-dialog").data("id", $(this).attr("id")).dialog("open");
-                })
+                div.click(function() {
+                    if ($(this).attr("class").indexOf('seed') < 0) {
+                        $("#" + cellId).css("background-color", "");
+                        cellId = $(this).attr("id");
+                        $(this).css("background-color", "red");
+                    }
+                });
             }
 
             div.addClass(bigCellIndexAt(i) % 2 == 0 ? "even" : "odd");
